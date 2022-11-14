07171c69bd113afd468c34e5c583766daeb1ce27
SanoferSameera <sanofersameera25@gmail.com>
2022-11-14 12:18:19 +0530

    sms debug

--- src/In.ProjectEKA.OtpService/Clients/GatewaySmsClient.cs
+++ src/In.ProjectEKA.OtpService/Clients/GatewaySmsClient.cs
@@ -1,9 +1,7 @@
-using System.Web;
+using System.Net.Http.Headers;
 
 namespace In.ProjectEKA.OtpService.Clients
 {
-    using System;
-    using System.Net;
     using System.Net.Http;
     using System.Threading.Tasks;
     using System.Runtime.Caching;
@@ -13,56 +11,70 @@ namespace In.ProjectEKA.OtpService.Clients
 	public class GatewaySmsClient : ISmsClient
     {
         private readonly SmsServiceProperties smsServiceProperties;
-        private readonly HttpClient client;
+        private readonly HttpClient httpClient;
         private MemoryCache cache;
 
         public GatewaySmsClient(SmsServiceProperties smsServiceProperties)
         {
             this.smsServiceProperties = smsServiceProperties;
-            client = new HttpClient();
+            httpClient = new HttpClient();
             cache = MemoryCache.Default;
         }
 
         public async Task<Response> Send(string phoneNumber, string message, string templateId)
         {
-            var phoneNumberWithCountryCode = phoneNumber;
-            if (phoneNumber.Contains('-'))
-                phoneNumberWithCountryCode = phoneNumber.Replace("+", string.Empty).Replace("-", String.Empty);
+            Log.Information("gateway sms client");
+            var request = new HttpRequestMessage(new HttpMethod("POST"), "https://api.d7networks.com/messages/v1/send");
 
-            try
-            {
-                var uriBuilder = new UriBuilder(smsServiceProperties.SmsApi);
-                uriBuilder.Port = -1;
-                var query = HttpUtility.ParseQueryString(uriBuilder.Query);
-                query["username"] = smsServiceProperties.ClientId;
-                query["pin"] = smsServiceProperties.ClientSecret;
-                query["message"] = message;
-                query["mnumber"] = phoneNumberWithCountryCode;
-                query["dlt_template_id"] = templateId;
-                query["signature"] = smsServiceProperties.Signature;
-                query["dlt_entity_id"] = smsServiceProperties.EntityId;
+            request.Headers.TryAddWithoutValidation("Accept", "application/json");
+            request.Headers.TryAddWithoutValidation("Authorization", "Bearer ");
 
-                uriBuilder.Query = query.ToString();
-                
-                var request = new HttpRequestMessage(HttpMethod.Get, uriBuilder.ToString());
+            request.Content =
+                new StringContent(
+                    "{\n\"messages\": [{\"channel\": \"sms\", \"recipients\": [\"+919865689295\"], \"content\": \"Greetings from D7 API\", \"msg_type\": \"text\", \"originator\": \"Bahmni\"}]");
+            request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
 
-                var response = await client
-                    .SendAsync(request)
-                    .ConfigureAwait(false);
-
-                var contents = await response.Content.ReadAsStringAsync();
-                Log.Information(contents);
-                if (response.StatusCode == (HttpStatusCode) 200)
-                    return new Response(ResponseType.Success, "Notification sent");
-                Log.Error(response.StatusCode,response.Content);
-            }
-            catch (Exception exception)
-            {
-                Log.Error(exception, exception.StackTrace);
-                return new Response(ResponseType.InternalServerError, "Unable to create otp message.");
-            }
-
-            return new Response(ResponseType.Success, "Error in sending notification");
+            var response = await httpClient.SendAsync(request);
+            Log.Information("SMS" + response);
+            return new Response(ResponseType.Success, "Notification sent");
+            // var phoneNumberWithCountryCode = phoneNumber;
+            // if (phoneNumber.Contains('-'))
+            //     phoneNumberWithCountryCode = phoneNumber.Replace("+", string.Empty).Replace("-", String.Empty);
+            //
+            // try
+            // {
+            //     var uriBuilder = new UriBuilder(smsServiceProperties.SmsApi);
+            //     uriBuilder.Port = -1;
+            //     var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+            //     query["username"] = smsServiceProperties.ClientId;
+            //     query["pin"] = smsServiceProperties.ClientSecret;
+            //     query["message"] = message;
+            //     query["mnumber"] = phoneNumberWithCountryCode;
+            //     query["dlt_template_id"] = templateId;
+            //     query["signature"] = smsServiceProperties.Signature;
+            //     query["dlt_entity_id"] = smsServiceProperties.EntityId;
+            //
+            //     uriBuilder.Query = query.ToString();
+            //     
+            //     var request = new HttpRequestMessage(HttpMethod.Get, uriBuilder.ToString());
+            //
+            //     var response = await client
+            //         .SendAsync(request)
+            //         .ConfigureAwait(false);
+            //
+            //     var contents = await response.Content.ReadAsStringAsync();
+            //     Log.Information(contents);
+            //     if (response.StatusCode == (HttpStatusCode) 200)
+            //         return new Response(ResponseType.Success, "Notification sent");
+            //     Log.Error(response.StatusCode,response.Content);
+            // }
+            // catch (Exception exception)
+            // {
+            //     Log.Error(exception, exception.StackTrace);
+            //     return new Response(ResponseType.InternalServerError, "Unable to create otp message.");
+            // }
+            //
+            // return new Response(ResponseType.Success, "Error in sending notification");
         }
     }
 }
--- src/In.ProjectEKA.OtpService/Clients/SmsClient.cs
+++ src/In.ProjectEKA.OtpService/Clients/SmsClient.cs
@@ -1,43 +1,57 @@
-using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace In.ProjectEKA.OtpService.Clients
 {
-	using System.Collections.Specialized;
-	using System.Net;
-	using System.Text;
 	using System.Threading.Tasks;
-	using System.Web;
 	using Common;
 	using Common.Logger;
 	using Microsoft.Extensions.Configuration;
-	using Newtonsoft.Json.Linq;
 
 	public class SmsClient : ISmsClient
     {
         private readonly IConfiguration configuration;
+        private readonly HttpClient httpClient;
 
-        public SmsClient(IConfiguration configuration)
+        public SmsClient(IConfiguration configuration, HttpClient httpClient)
         {
-            this.configuration = configuration;
+	        this.configuration = configuration;
+	        this.httpClient = httpClient;
         }
 
         public async Task<Response> Send(string phoneNumber, string message, string templateID)
         {
-            var notification = HttpUtility.UrlEncode(message);
-            using var webClient = new WebClient();
-            var response = await webClient.UploadValuesTaskAsync("https://api.textlocal.in/send/",
-                new NameValueCollection
-                {
-                    {"apikey" , configuration.GetConnectionString("TextLocaleApiKey")},
-                    {"numbers" , phoneNumber},
-                    {"message" , notification},
-                    {"sender name" , "HCMNCG"},
-                });
-            var json = JObject.Parse(Encoding.UTF8.GetString(response));
-            Log.Information((string)json["status"] == "success" ? "Success in sending notification" :
-                (string)json["errors"][0]["message"]);
-            return (string)json["status"] == "success" ?  new Response(ResponseType.Success,"Notification sent") 
-                    :new Response(ResponseType.Success, (string)json["errors"][0]["message"]);
+            Log.Information("sms client");
+	        var request = new HttpRequestMessage(new HttpMethod("POST"), "https://api.d7networks.com/messages/v1/send");
+
+	        request.Headers.TryAddWithoutValidation("Accept", "application/json");
+	        request.Headers.TryAddWithoutValidation("Authorization", "Bearer ");
+
+	        request.Content =
+		        new StringContent(
+			        "{\n\"messages\": [{\"channel\": \"sms\", \"recipients\": [\"+919865689295\"], \"content\": \"Greetings from D7 API\", \"msg_type\": \"text\", \"originator\": \"Bahmni\"}]");
+	        request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+
+	        var response = await httpClient.SendAsync(request);
+	        Log.Information("SMS" + response);
+	        return new Response(ResponseType.Success, "Notification sent");
+
+
+	        // var notification = HttpUtility.UrlEncode(message);
+	        // using var webClient = new WebClient();
+	        // var response = await webClient.UploadValuesTaskAsync("https://api.textlocal.in/send/",
+	        //     new NameValueCollection
+	        //     {
+	        //         {"apikey" , configuration.GetConnectionString("TextLocaleApiKey")},
+	        //         {"numbers" , phoneNumber},
+	        //         {"message" , notification},
+	        //         {"sender name" , "HCMNCG"},
+	        //     });
+	        // var json = JObject.Parse(Encoding.UTF8.GetString(response));
+	        // Log.Information((string)json["status"] == "success" ? "Success in sending notification" :
+	        //     (string)json["errors"][0]["message"]);
+	        // return (string)json["status"] == "success" ?  new Response(ResponseType.Success,"Notification sent") 
+	        //         :new Response(ResponseType.Success, (string)json["errors"][0]["message"]);
         }
     }
 }
